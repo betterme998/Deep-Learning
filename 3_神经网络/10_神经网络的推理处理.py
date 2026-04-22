70d42d53b344092b7961b1118d9b268e8cc579dd
betterme998 <betterme998@163.com>
2026-04-22 17:01:02 +0800

    手写数字识别

--- 3_神经网络/10_神经网络的推理处理.py
+++ 3_神经网络/10_神经网络的推理处理.py
@@ -16,5 +16,22 @@ from typing import Any # 类型提示和类型注解的功能
 
 import numpy as np # 科学计算机库，处理数组数据
 import numpy.typing as npt # 导入 NumPy 的类型注解工具并起一个简短的别名，主要用于静态类型检查场景，让开发者能清晰地标注数组、标量、数据类型等对象的类型
+# 1.使用pathlib 构建路径
+# 将父目录添加到系统路径中
+# Path(__file__).resolve()当前文件的绝对路径
+# parent 获取父级目录
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+print(str(Path(__file__).resolve().parent.parent))
+
 from dataset.mnist import load_mnist # 从dataset包中导入MNIST数据集加载函数
-from common.functions import sigmoid, softmax
+from common.functions import sigmoid, softmax # 激活函数
+
+def get_data():
+  """
+  获取经过预处理的MNIST测试数据.
+  参数 normalize=True 表示将像素归一化到[0,1] 区间
+  flatten=True 表示将 28x28 的图像展平为一维数组（784个元素）；
+  one_hot_label=False 表示标签以原始数字形式(0~9) 存储，而非one-hot编码.[0,0,1,0,0,0,0,0,0,0] = 2
+  """
+
--- common/functions.py
+++ common/functions.py
@@ -1,1 +1,25 @@
-""""""
+"""
+激活函数
+"""
+import numpy as np
+
+def sigmoid(x):
+  #避免指数运算溢出
+  # 正数区域：使用负指数形式，exp(-x) 很小不溢出
+  # 负数区域：使用正指数形式，exp(x) 很小不溢出
+  # 1 / (1 + np.exp(-x))和np.exp(x) / (1 + np.exp(x)))这两个写法在数学上完全等价
+  return np.where(x >= 0,
+                  1 / (1 + np.exp(-x)), #此时exp(-x) <= 1
+                  np.exp(x) / (1 + np.exp(x))) #此时exp(x) < 1
+
+# 常用于多分类任务中，把一组实数转换成概率分布（所有输出在0到1之间且和为1）
+def softmax(x):
+  #axis 指定要操作的轴（维度） 
+  #-1 在 Python 索引中表示最后一个轴。代表沿着列方向
+  # a = np.array([[1, 2, 3],
+              # [4, 5, 6]])
+  # np.max(a, axis=-1) # 返回 [3, 6]
+  # np.max(a, axis=-1, keepdims=True) # 返回 [[3], [6]] 
+  #keepdims=True 保留原始数组的维度，而不是默认的降维。 
+  x = x - np.max(x, axis=-1, keepdims=True)
+  return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims= True)
