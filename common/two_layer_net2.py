c3a2347d4084cd6dfe5ed07fd3a6d03ef96c81b4
betterme <betterme998@163.com>
2026-05-12 22:51:48 +0800

    手写数字识别

--- common/two_layer_net2.py
+++ common/two_layer_net2.py
@@ -53,3 +53,27 @@ class TwoLayerNet:
     grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
     grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
+    return grads
+
+  def gradient(self, x, t):
+    # forward
+    self.loss(x, t)
+
+    # backward
+    dout = 1
+    dout = self.lastLayer.backward(dout)
+
+    layers = list(self.layers.values())
+    layers.reverse()
+    for layer in layers:
+      dout = layer.backward(dout)
+
+    # 设定
+    grads = {}
+    grads['W1'] = self.layers['Affine1'].dW
+    grads['b1'] = self.layers['Affine1'].db
+    grads['W2'] = self.layers['Affine2'].dW
+    grads['b2'] = self.layers['Affine2'].db
+
+    return grads
+
